685af689d0e732bc1ea42dd9fd6a0da269c15194
Anartz Mugika <mugan86@gmail.com>
2019-02-09 20:48:02 +0100

    Optimize

--- src/app/constants/components.ts
+++ src/app/constants/components.ts
@@ -1,12 +1,13 @@
-import { AwardsComponent } from './../components/sections/awards/awards.component';
 import { AppComponent } from '../app.component';
 import { NavbarComponent } from '../components/main/navbar/navbar.component';
 import { ContentComponent } from '../components/main/content/content.component';
-import { AboutComponent } from '../components/sections/about/about.component';
-import { ExperienceComponent } from '../components/sections/experience/experience.component';
-import { EducationComponent } from '../components/sections/education/education.component';
-import { SkillsComponent } from '../components/sections/skills/skills.component';
-import { InterestsComponent } from '../components/sections/interests/interests.component';
+import { AboutComponent } from '../components/main/content/sections/about/about.component';
+import { ExperienceComponent } from '../components/main/content/sections/experience/experience.component';
+import { EducationComponent } from '../components/main/content/sections/education/education.component';
+import { SkillsComponent } from '../components/main/content/sections/skills/skills.component';
+import { InterestsComponent } from '../components/main/content/sections/interests/interests.component';
+import { AwardsComponent } from '../components/main/content/sections/awards/awards.component';
+
 
 export const COMPONENTS = [
     AppComponent,
---
+++ src/app/interfaces/shared-element.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * Define properties to Shared Elements
+ */
+export interface SharedElement {
+    url: string;
+    path: string;
+    icon: string;
+}
